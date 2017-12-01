dcab777385cb7010b8b7257bdbefd49365eb4cc8
Kent Miller <kmiller@MSC.local>
2017-12-01 11:35:36 -0600

    Add lovely buttons to the control page

--- src/public/javascript/control.js
+++ src/public/javascript/control.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const setupSpotifyButtons = () => {
+  console.log("called");
   document.getElementById('sptPlayBtn').addEventListener('click', () => {window.music.sptPlay();});
   document.getElementById('sptPauseBtn').addEventListener('click', () => {window.music.sptPause();});
   document.getElementById('sptPrevBtn').addEventListener('click', () => {window.music.sptPreviousTrack();});
