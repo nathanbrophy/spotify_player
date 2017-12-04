995e8bf23022180740fba5c60014563b6a466f62
Jade Thornton <jade@jmthornton.net>
2017-12-03 21:19:12 -0600

    move mopidy to main page

--- src/public/javascript/index.js
+++ src/public/javascript/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const setupSpotifyButtons = () => {
+  console.log("called");
+  document.getElementById('sptPlayBtn').addEventListener('click', () => {window.music.sptPlay();});
+  document.getElementById('sptPauseBtn').addEventListener('click', () => {window.music.sptPause();});
+  document.getElementById('sptPrevBtn').addEventListener('click', () => {window.music.sptPreviousTrack();});
+  document.getElementById('sptNextBtn').addEventListener('click', () => {window.music.sptNextTrack();});
+  document.getElementById('mopidy').style.display = 'block';
+}
+
+if (window.music) {
+  setupSpotifyButtons();
+} else {
+  document.getElementById('mopidy').addEventListener('spotifyready', () => {
+    setupSpotifyButtons();
+  });
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,4 @@ router.get('/', (req, res, next) => {
   res.render('index', {title: 'Mavboard'});
 });
 
-// render control page
-router.get('/control', (req, res, next) => {
-  res.render('control', {title: 'Mavboard Control Panel'});
-});
-
 module.exports = router;
