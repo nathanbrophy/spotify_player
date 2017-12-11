dc70504cb4a4966c6a8f13794fc43d746c602d4f
nati-natnael <natnaelger@gmail.com>
2017-12-11 10:20:35 -0600

    update text color for black background

--- src/public/javascript/nextrip.js
+++ src/public/javascript/nextrip.js
@@ -1,21 +1,21 @@
 var $ = (id) => {
   var ele = null;
   try {ele = document.getElementById(id);}
-  catch (err) {console.log(err);}	
+  catch (err) {console.log(err);}
   return ele;
 };
 
 var $$ = (clas) => {
   var ele = null;
   try {ele = document.getElementsByClassName(clas);}
-  catch (err) {console.log(err);}	
+  catch (err) {console.log(err);}
   return ele;
 };
 
 /**
  * start setInterval event to diplay
  * bus times on window load.
- * 
+ *
  * Note:
  *  add stop ids need to desplay in
  *  stopIDs array.
@@ -24,14 +24,14 @@ let displayBusTimes = () => {
   //13207
   var stopIDs = [16325];
   var container = $('bus');
-  
+
   /*
   update no more less than every 30 sec.
   recommended by metrotransit.org web services
    */
   stopIDs.forEach ((stopID) => {
     loadTransitTimes(stopID, container);
-    
+
     // Clears container content every time it fires
     setInterval (() => {container.innerHTML = ''; loadTransitTimes(stopID, container);}, 35000);
   });
@@ -40,7 +40,7 @@ let displayBusTimes = () => {
 /**
  * start setInterval event to
  * diplay train times on window load
- * 
+ *
  * Note:
  *  add stop ids need to desplay in
  *  stopIDs array.
@@ -48,11 +48,11 @@ let displayBusTimes = () => {
 let displayTrainTimes = () => {
   var stopIDs = [56001, 56043];
   var container = $('train');
-  
+
   // update no more less than every 30 sec. recommended by metrotransit.org web services
   stopIDs.forEach ((stopID) => {
     loadTransitTimes(stopID, container);
-    
+
     // Clears container content every time it fires
     setInterval (() => {container.innerHTML = ''; loadTransitTimes(stopID, container);}, 35000);
   });
@@ -68,32 +68,32 @@ let displayTrainTimes = () => {
  */
 let loadTransitTimes = (stopID, container) => {
   var http = new XMLHttpRequest();
-  
+
   // Add spinner
   container.innerHTML = '<img id="spinner_' + stopID + '" src="../imgs/spinner.gif" alt="spinner gif">';
   http.onreadystatechange = () => {
     if (http.readyState == 4 && http.status == 200) {
       var uniqueRoute = [];
-      
+
       var response = http.responseXML;
       var x = response.getElementsByTagName("NexTripDeparture");
-      
-      var content = '<div id="' + stopID + '">Stop ID: ' + stopID;
+
+      var content = '<div class="transit_stop" id="' + stopID + '">Stop ID: ' + stopID;
       for (var i = 0; i < x.length; i++) {
         var route = x[i].getElementsByTagName('Route')[0];
         route = route.innerHTML;
-        
+
         if (!uniqueRoute.includes(route)) {
           content += createDepartureDiv(x[i]);
-          
+
           uniqueRoute.push(route);
         }
       }
       content += '</div>';
-      
+
       // Remove spinner
       let spinner = $('spinner_' + stopID);
-      
+
       /**
       There may be two stopIDs in one container.
       If so, ones coming after the first will
@@ -101,11 +101,11 @@ let loadTransitTimes = (stopID, container) => {
       won't exist.
        */
       if (spinner != null) {container.removeChild(spinner);}
-            
+
       container.innerHTML += content;
     }
   };
-  
+
   var url = "http://svc.metrotransit.org/NexTrip/" + stopID;
   http.open("GET", url, true);
   http.send();
@@ -117,26 +117,26 @@ let loadTransitTimes = (stopID, container) => {
  */
 let createDepartureDiv = (departure) => {
   var div = '<div class="departure">';
-  
+
   try {
-    
+
     var depRouteArr = departure.getElementsByTagName('Route');
     var depDirArr   = departure.getElementsByTagName('RouteDirection');
     var depTimeArr  = departure.getElementsByTagName('DepartureText');
     var depDescArr  = departure.getElementsByTagName('Description');
-    
+
     var depRoute = depRouteArr != null ? depRouteArr[0].innerHTML : 'No Route';
     var depDir   = depDirArr   != null ? depDirArr[0].innerHTML   : 'No Route Direction';
     var depTime  = depTimeArr  != null ? depTimeArr[0].innerHTML  : 'No Departure Time';
     var depDesc  = depDescArr  != null ? depDescArr[0].innerHTML  : 'No Description';
-    
+
     div += '<span class="dep_trans">';
     div += depRoute + ' ';
     div += depDir;
     div += '</span><br>';
-    
+
     div += '<span class="dep_desc">' + depDesc + '</span><br>';
-    
+
     if (depTime.includes('Min')) {
       div += '<span class="dep_time">Next in: ' + depTime + ' </span>';
     } else {
@@ -145,8 +145,8 @@ let createDepartureDiv = (departure) => {
   } catch (err) {
     console.log(err);
   }
-  
+
   div += '</div>';
-  
+
   return div;
 };
