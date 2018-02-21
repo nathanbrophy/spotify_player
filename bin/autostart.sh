9f63bebb82059f8d8ad3455e63417e6816758e6d
Jason Timmerman <jtimmerman@mavericksoftware.com>
2018-02-21 12:30:03 -0600

    fix script

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -1,4 +1,2 @@
 #This script runs along with a few other commands located in ~/.config/lxsession/LXDE-pi/autostart in order to automatically start the web server and open browser
-cd $HOME/mavboard 
-&& git pull 
-&& npm start
+cd $HOME/mavboard && git pull && npm start
