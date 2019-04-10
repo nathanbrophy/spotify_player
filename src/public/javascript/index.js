1fc798e7e0da6bcddf1dd54acdf17387c2b13a83
Nathan Brophy <nbrophy@MSC.local>
2019-04-10 12:05:51 -0500

    Adding in a bunch of cool music interaction features

--- src/public/javascript/index.js
+++ src/public/javascript/index.js
@@ -12,3 +12,29 @@ function displayDynamicContent() {
 	displayAllTimes();
 	displayWeather();
 }
+
+function toggleSpotifyDiv() {
+	var containerAlbumArt = document.getElementById("spotify-info-container");
+	var containerList = document.getElementById("spotify-interative-list-container");
+
+	var listDisplayed = containerList.classList.contains("active");
+	if (!listDisplayed) {
+		containerAlbumArt.classList.remove("active");
+		containerAlbumArt.classList.add("hidden");
+
+		containerList.classList.remove("hidden");
+		containerList.classList.add("active");
+
+		document.getElementById('toggle-list-link').innerHTML = "Toggle Album Info";
+	}
+	else {
+		containerAlbumArt.classList.remove("hidden");
+		containerAlbumArt.classList.add("active");
+
+		containerList.classList.remove("active");
+		containerList.classList.add("hidden");
+
+		document.getElementById('toggle-list-link').innerHTML = "Toggle List";
+	}
+	generateList();
+}
