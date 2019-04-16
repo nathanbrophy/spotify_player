e661e706b6280a8892431025a24acb4740cd7ada
Nathan Brophy <nbrophy@MSC.local>
2019-04-16 15:23:31 -0500

    Finished adding in the rest of the spotify changes

--- src/public/javascript/index.js
+++ src/public/javascript/index.js
@@ -1,15 +1,8 @@
 'use strict';
 
 var containers;
-const setupSpotifyButtons = () => {
-  document.querySelector('#sptPlayBtn').addEventListener('click', () => {music.sptPlay();});
-  document.querySelector('#sptPauseBtn').addEventListener('click', () => {music.sptPause();});
-  document.querySelector('#sptPrvBtn').addEventListener('click', () => {music.sptPreviousTrack();});
-  document.querySelector('#sptNextBtn').addEventListener('click', () => {music.sptNextTrack();});
-  spotDiv.style.display = 'block';
-}
-
 function displayDynamicContent() {
+	window.playlist = false;
 	displayAllTimes();
 	displayWeather();
 
@@ -65,30 +58,4 @@ function toggleSpotifyDiv() {
 		document.getElementById('toggle-list-link').innerHTML = "Toggle List";
 	}
 	generateList();
-}
-
-function changeTheme(theme) {
-	var link = document.getElementById('theme');
-	var url = '/stylesheets/' + theme + "_theme.css";
-	link.href = url;
-
-	var _ctheme = document.getElementById('colorful-theme');
-	var _itheme = document.getElementById('light-theme');
-	var _dtheme = document.getElementById('dark-theme');
-	switch (theme) {
-		case 'colorful':
-			_ctheme.classList.add('current-theme');
-			_itheme.classList.remove('current-theme');
-			_dtheme.classList.remove('current-theme');
-			break;
-		case 'dark':
-			_dtheme.classList.add('current-theme');
-			_itheme.classList.remove('current-theme');
-			_ctheme.classList.remove('current-theme'); 
-			break;
-		default:
-			_itheme.classList.add('current-theme');
-			_ctheme.classList.remove('current-theme');
-			_dtheme.classList.remove('current-theme');
-	}
 }
--- src/public/javascript/spotify.js
+++ src/public/javascript/spotify.js
@@ -7,6 +7,25 @@
 // mopidy.tracklist
 // mopidy.playlists
 // mopidy.library
+var currentTime, totalTime, paused, intervalfn; // use storage uphere
+
+var xmlH = new XMLHttpRequest();
+xmlH.onreadystatechange = function() {
+  if (xmlH.readyState == 4 && xmlH.status == 200) {
+    var data = JSON.parse(xmlH.responseText);
+    currentTime = parseInt(data.curr);
+    totalTime = parseInt(data.total);
+    updateTimeBar();
+    intervalfn = setInterval(function() {
+      if (!paused) {
+        currentTime++;
+        updateTimeBar();
+      }
+    }, 1000);
+  }
+}
+xmlH.open("GET", "/currentTime", true);
+xmlH.send();
 
 class SpotifyController {
   rpcURL: string;
@@ -65,58 +84,59 @@ class SpotifyController {
         // after this, syncState will handle updating
         mopidy.on('state:online', () => {
           this.state.online = true;
-
-          mopidy.tracklist.setConsume(true);
-          var consume = document.getElementById('consume-toggle');
-          if (!consume.classList.contains('on')) {
-            consume.classList.add('on');
-          }
-          var volume = document.getElementById('music-volume-control').value;
-          mopidy.mixer.setVolume(parseInt(volume));
-          mopidy.playback.getCurrentTrack().then((track) => {
-            this.state.currentTrack = track;
-            if (this.state.currentTrack) {
-              console.log(`Currently playing ${this.state.currentTrack.name} by ${this.state.currentTrack.artists[0].name}`);
-              mopidy.library.getImages([track.uri]).then(result => updateCoverArt(track.uri, result));
-            } else {
-              mopidy.playlists.getPlaylists().then(function (playlists) {
-                console.log("Loading playlist");
-                var playlist;
-                for (var i = 0; i < playlists.length; i++) {
-                  if (playlists[i].name == "MSP Fall 2018") {
-                    playlist = playlists[i];
+          if (!window.playlist) {
+            mopidy.tracklist.setConsume(true);
+            var consume = document.getElementById('consume-toggle');
+            if (consume && !consume.classList.contains('on')) {
+              consume.classList.add('on');
+            }
+            var volume = document.getElementById('music-volume-control').value;
+            mopidy.mixer.setVolume(parseInt(volume));
+            mopidy.playback.getCurrentTrack().then((track) => {
+              this.state.currentTrack = track;
+              if (this.state.currentTrack) {
+                console.log(`Currently playing ${this.state.currentTrack.name} by ${this.state.currentTrack.artists[0].name}`);
+                mopidy.library.getImages([track.uri]).then(result => updateCoverArt(track.uri, result));
+              } else {
+                mopidy.playlists.getPlaylists().then(function (playlists) {
+                  console.log("Loading playlist");
+                  var playlist;
+                  for (var i = 0; i < playlists.length; i++) {
+                    if (playlists[i].name == "MSP Fall 2018") {
+                      playlist = playlists[i];
+                    }
                   }
-                }
-                // Modification of the Fisher-Yates shuffle algorithm
-                if (playlist != null) {
-                  var currentI = playlist.tracks.length, tempVal, randomI;
-                  while (0 != currentI) {
-                    randomI = Math.floor(Math.random() * currentI);
-                    currentI -= 1;
+                  // Modification of the Fisher-Yates shuffle algorithm
+                  if (playlist != null) {
+                    var currentI = playlist.tracks.length, tempVal, randomI;
+                    while (0 != currentI) {
+                      randomI = Math.floor(Math.random() * currentI);
+                      currentI -= 1;
 
-                    tempVal = playlist.tracks[currentI];
-                    playlist.tracks[currentI] = playlist.tracks[randomI];
-                    playlist.tracks[randomI] = tempVal;
-                  }
-                  mopidy.tracklist.add(playlist.tracks).then(function (tiTracks) {
-                    mopidy.playback.play(tiTracks[0]).then(function () {
-                      console.log("Loaded the playlist.");
+                      tempVal = playlist.tracks[currentI];
+                      playlist.tracks[currentI] = playlist.tracks[randomI];
+                      playlist.tracks[randomI] = tempVal;
+                    }
+                    mopidy.tracklist.add(playlist.tracks).then(function (tiTracks) {
+                      mopidy.playback.play(tiTracks[0]).then(function () {
+                        console.log("Loaded the playlist.");
+                      });
                     });
-                  });
-                }
-              }).done();
-            }
-          });
-          mopidy.playback.getCurrentTrack().then((track) => {
-            this.state.currentTrack = track;
-            if (this.state.currentTrack) {
-              console.log(`Currently playing ${this.state.currentTrack.name} by ${this.state.currentTrack.artists[0].name}`);
-              document.getElementById('currSong').innerHTML = '' + track.name + ' -- ' + track.artists[0].name;
-              mopidy.library.getImages([track.uri]).then(result => updateCoverArt(track.uri, result));
-            } else {
-              console.log('No track currently playing');
-            }
-          });
+                  }
+                }).done();
+              }
+            });
+            mopidy.playback.getCurrentTrack().then((track) => {
+              this.state.currentTrack = track;
+              if (this.state.currentTrack) {
+                console.log(`Currently playing ${this.state.currentTrack.name} by ${this.state.currentTrack.artists[0].name}`);
+                document.getElementById('currSong').innerHTML = '' + track.name + ' -- ' + track.artists[0].name;
+                mopidy.library.getImages([track.uri]).then(result => updateCoverArt(track.uri, result));
+              } else {
+                console.log('No track currently playing');
+              }
+            });
+          }
           resolve(mopidy);
         });
       } catch (e) {
@@ -167,11 +187,28 @@ class SpotifyController {
       case 'event:trackPlaybackStarted':
         window.music.mopidy.playback.getCurrentTrack().then((track) => {
           window.music.state.currentTrack = track;
+          console.log("currently playling: ", track);
+          if (intervalfn) clearInterval(intervalfn);
+          totalTime = parseInt(track.length / 1000);
+          currentTime = 0;
+          updateTimeBar();
+          intervalfn = setInterval(function() {
+            if (!paused) {
+              currentTime++;
+              updateTimeBar();
+            }
+          }, 1000);
           document.getElementById('currSong').innerHTML = '' + track.name + ' -- ' + track.artists[0].name;
 	        window.music.mopidy.library.getImages([track.uri]).then(result => updateCoverArt(track.uri, result));
         });
         break;
 
+      case 'event:trackPlaybackResumed':
+        paused = false;
+        break;
+      case 'event:trackPlaybackPaused':
+        paused = true;
+        break;
       // Ensure volumn stays bearable
       case 'event:volumeChanged':
         console.log("Volume changed");
@@ -208,4 +245,31 @@ function updateCoverArt(trackUri, imgs) {
 	elem.setAttribute("src", image.uri);
 	elem.setAttribute("height", image.height);
 	elem.setAttribute("width", image.width);
+}
+
+function updateTimeBar() {
+  var displayCurrentTime = '';
+  var _copy = currentTime;
+  var _minutes = parseInt(_copy / 60);
+  var _copy = _copy - (_minutes * 60);
+  var _seconds = _copy;
+  if (_seconds <= 9) {
+    _seconds = '0' + _seconds;
+  }
+  displayCurrentTime = `${_minutes}:${_seconds}`;
+  document.getElementById('current-time').innerHTML = displayCurrentTime;
+
+  var displayTotalTime = '';
+  _copy = totalTime;
+  _minutes = parseInt(_copy / 60);
+  _copy = _copy - (_minutes * 60);
+  _seconds = _copy;
+  if (_seconds <= 9) {
+    _seconds = '0' + _seconds;
+  } 
+  displayTotalTime = `${_minutes}:${_seconds}`;
+  document.getElementById('total-time').innerHTML = displayTotalTime;
+
+  var width = (currentTime / totalTime) * 100;
+  document.getElementById('time-bar').style.width = width + '%';
 }
